fc9d87d419517c9fa8b3394a72176f11a67e1571
Juan Haddad <haddadjuan0@gmail.com>
2022-05-19 11:47:10 -0300

    v1.0

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,6 +1,6 @@
 from funcoes import *
 
-# Calculadora básica em python feita por: Juan Hernandes
+# Calculadora básica em python
 
 menu('CALCULADORA')
 op = operacoes() # Call the def which ask for a option to make basics operations
@@ -28,33 +28,49 @@ elif op == 6:
     menu('RAIZ')
     v = raiz()
 
+elif op == 7:
+    menu('FATORIAL')
+    v = fat()
+
 print(f'O resultado dessa operação foi \033[36m{v:.2f}\033[m')
 while True:
+    try:
+        v = int(v)
+    except:
+        pass
     r = str(input(f'Quer partir pra outra operação com o valor {v:.2f}? [S/N] ')).upper().strip()[0]
     if r not in 'SN':
         r = str(input('ERRO! Digite S ou N.'))
     if r == 'N':
         break
+
     print('-'*32)
     op = operacoes(v)
+
     if op == 1:
         menu('SOMA')
         v = soma(v)
+
     elif op == 2:
         menu('SUBTRAÇÃO')
         v = subtracao(v)
+
     elif op == 3:
         menu('MULTIPLICAÇÃO')
         v = multiplicacao(v)
+
     elif op == 4:
         menu('DIVISÃO')
         v = divisao(v)
+
     elif op == 5:
         menu('POTENCIAÇÃO')
         v = potenciacao(v)
+
     elif op == 6:
         menu('RAIZ')
         v = raiz(v)
+
     print(f'O resultado dessa operação foi \033[36m{v:.2f}\033[m')
 
 print(f'\nSeu valor final é --> \033[35m{v:.2f}\033[m\n<< VOLTE SEMPRE >>')
--- Calculadora/funcoes/__init__.py
+++ Calculadora/funcoes/__init__.py
@@ -1,12 +1,17 @@
 import math
+from time import sleep
 
-def menu(msg):
+def menu(msg): # Cria uma interface.
     print("-" * 32)
     print(f'{msg}'.center(32))
     print("-" * 32)
 
-def operacoes(a=0):
+def operacoes(a=0): # V
     operacoes = ['Soma', 'Subtração', 'Multiplicação', 'Divisão', 'Potenciação', 'Raiz']
+    if type(a) == int:
+        operacoes.append('Fatorial')
+    else:
+        operacoes.remove('Fatorial')
     for i, n in enumerate(operacoes):
         print(f'\033[32m{i + 1}\033[m - {n}')
     if a == 0:
@@ -23,6 +28,8 @@ def soma(a=0):
     else:
         a = float(input('Digite o número a somar: '))
         b = float(input(f'Por quanto você quer somar {a}? '))
+    print('Calculando...')
+    sleep(1)
     c = a + b
     return c
 
@@ -32,6 +39,8 @@ def subtracao(a=0):
     else:
         a = float(input('Digite o número a ser subtraído: '))
         b = float(input(f'Por quanto você quer subtrair {a}? '))
+    print('Calculando...')
+    sleep(1)
     c = a - b
     return c
 
@@ -41,6 +50,8 @@ def multiplicacao(a=0):
     else:
         a = float(input('Digite o número a ser multiplicado: '))
         b = float(input(f'Por quanto você quer multiplicar {a}? '))
+    print('Calculando...')
+    sleep(1)
     c = a * b
     return c
 
@@ -50,6 +61,8 @@ def divisao(a=0):
     else:
         a = float(input('Digite o número a ser dividido: '))
         b = float(input(f'Por quanto você quer dividir {a}? '))
+    print('Calculando...')
+    sleep(1)
     c = a / b
     return c
 
@@ -59,6 +72,8 @@ def potenciacao(a=0):
     else:
         a = float(input('Digite o número a ser elevado: '))
         b = float(input(f'Por quanto você quer elevar {a}? '))
+    print('Calculando...')
+    sleep(1)
     c = a ** b
     return c
 
@@ -70,5 +85,25 @@ def raiz(a=0):
         a = float(input('Digite o número a ser o radicando: '))
         b = float(input(
             f'Qual o índice para o radicando {a}? (\033[32m2\033[m = Raiz quadrada, \033[32m3\033[m = Raiz cubica, ...)'))
+    print('Calculando...')
+    sleep(1)
     c = a ** (1 / b)
+    return c
+
+def fat():
+    while True:
+        a = input('Digite um número inteiro para ter seu fatorial: ')
+        try:
+            a = int(a)
+        except:
+            print(f'ERRO! Digite somente números inteiros! ')
+        else:
+            print('Calculando...')
+            sleep(1)
+        if type(a) == int:
+            break
+    c = 1
+    while a != 1:
+        c = c * a
+        a -= 1
     return c
